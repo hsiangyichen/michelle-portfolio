9508681020a2a8b18e28a9c3094b127d4467a0f9
hsiangyichen <hsiangyi1025@gmail.com>
2024-09-07 15:30:53 +0800

    feat: adjust box model

--- src/app/pages/Intro.js
+++ src/app/pages/Intro.js
@@ -25,11 +25,11 @@ const Intro = () => {
     <>
       <div
         id="intro"
-        className="sm:h-[80vh] py-6 items-center md:justify-center overflow-x-hidden flex flex-col md:flex-row w-full gap-12 md:gap-0"
+        className="sm:h-[80vh] py-10 items-center md:justify-center overflow-x-hidden flex flex-col md:flex-row w-full gap-12 md:gap-0"
       >
         <div className="flex flex-col px-4 z-10 md:relative md:left-20 lg:left-28 xl:left-28 2xl:left-20 w-full xxs:max-w-fit">
           <Fade direction="down" duration={800} delay={500} triggerOnce={true}>
-            <h1 className="text-2xl xxs:text-3xl xxs:leading-10 xs:text-3xl md:text-4xl xl:text-5xl xxs:mb-1 xs:mb-2 md:mb-2.5 xl:mb-5 2xl:mb-7">
+            <h1 className="text-2xl xxs:text-4xl xxs:leading-10 xs:text-4xl md:text-4xl xl:text-5xl xxs:mb-1 xs:mb-2 md:mb-2.5 xl:mb-5 2xl:mb-7">
               Hi, I{`'`}m <b className="font-semibold">Michelle</b>
               , <br />
             </h1>
